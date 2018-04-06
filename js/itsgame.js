56d9c5c9606ed790fb1fe2c6a5e1ec7eedeba4aa
Carenalga <mapedorr@gmail.com>
2018-04-05 23:09:54 -0500

    Fixed bug while saving game

--- js/itsgame.js
+++ js/itsgame.js
@@ -3778,7 +3778,7 @@ BasicGame.Game.prototype.saveGame = function (data) {
     return;
   }
 
-  this.saveGame(data);
+  localStorage.setItem("oh-my-blob", data);
 };
 
 BasicGame.Game.prototype.showPlayerDialogue = function (immediateHide) {
